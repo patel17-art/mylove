7f4a0aa6ca504deff913c1b39056a4fc2313195d
Tushar Patel <1dt18is107.tushar.a.patel@gmail.com>
2026-01-19 18:40:35 +0530

    Replace old files with new project files

--- js/functions.js
+++ js/functions.js
@@ -6,8 +6,8 @@ var clientHeight = $(window).height();
 $(function () {
     // setup garden
     $loveHeart = $("#loveHeart");
-    var offsetX = $loveHeart.width() / 2;
-    var offsetY = $loveHeart.height() / 2 - 55;
+    offsetX = $loveHeart.width() / 2;
+    offsetY = $loveHeart.height() / 2 - 55;
     $garden = $("#garden");
     gardenCanvas = $garden[0];
     gardenCanvas.width = $("#loveHeart").width();
